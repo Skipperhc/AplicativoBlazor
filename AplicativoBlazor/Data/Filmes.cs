c8d74c82da1fea3dd2aa0f4786a0a850213c53fa
Vitor Hainosz <vitorhainosz2@gmail.com>
2020-05-07 21:16:59 -0300

    Initial commit
    Aplicativo base usando Blazor

---
+++ AplicativoBlazor/Data/Filmes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AplicativoBlazor.Data {
+    public class Filmes {
+        public string Nome { get; set; }
+        public DateTime Lancamento { get; set; }
+
+    }
+}
